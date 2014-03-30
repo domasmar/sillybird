f3dd04dc335c438bd2b888f85670a36c6215e164
OrangeCup <domas933@gmail.com>
2014-03-30 23:12:46 +0300

    Columns added. Still no collision

--- src/flappy.py
+++ src/flappy.py
@@ -1,11 +1,12 @@
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.properties import NumericProperty, ObjectProperty, \
-    ReferenceListProperty
+    ReferenceListProperty, ListProperty
 from kivy.uix.widget import Widget
 from kivy.vector import Vector
-from kivy.graphics import Color, Line
+from kivy.graphics import Color, Line, Rectangle
 import math
+from random import randint
 
 class FlappyBird(Widget):
     x_step = 1
@@ -15,7 +16,7 @@ class FlappyBird(Widget):
     points = []
 
     def function(self, x):
-        return x * x * (-0.05)
+        return x * x * (-0.1)
 
     def next_position(self):
         self.velocity_y = self.function(self.x_pos + 1) - \
@@ -31,28 +32,74 @@ class FlappyBird(Widget):
         self.velocity_y = 0
 
 class FlappyColumn(Widget):
-    gap = NumericProperty(80)
-    max_c = NumericProperty(0)
-    min_c = NumericProperty(0)
-    position_x = NumericProperty(0)
-    position_y = NumericProperty(0)
-    position = ReferenceListProperty(position_x, position_y)
+    gap = 100
+    max_c = 0
+    min_c = 0
+    top_column_position = []
+    bottom_column_position = []
 
-    def new(self):
-        print "New column"
+    def __init__(self, game):
+        super(FlappyColumn, self).__init__()
+        self.game = game
+        with self.game.canvas:
+            Color(randint(10, 100) / 100.0, randint(10, 100) / 100.0, randint(10, 100) / 100.0)
+            self.rect_t = Rectangle(pos=[0,0], size= [30, self.game.height])
+            self.rect_b = Rectangle(pos=[0,0], size= [30, self.game.height])
+
+    def repaint(self):
+        with self.game.canvas:
+            Color(.98, .98, .0)
+            self.rect_t.pos = self.top_column_position
+            self.rect_b.pos = self.bottom_column_position
+
+    def new(self, diff):
+        self.min_c = randint(self.game.height/2 - diff/2, self.game.height/2 + diff/2)
+        self.max_c = self.min_c + self.gap
+        self.top_column_position = [self.game.width, self.max_c]
+        self.bottom_column_position = [self.game.width, self.min_c - self.game.height]
+        self.repaint()
 
     def update(self):
-        print "update"
+        self.top_column_position[0] -= 3
+        self.bottom_column_position[0] -= 3
+        self.repaint()
+        self.collision()
+
+    def collision(self):
+        if self.game.collide_widget(self.game.bird) or self.game.collide_widget(self.game.bird):
+            pass
+            # print "Kolizija"
+
+class FlappyColumns():
+    columns = []
+
+    def __init__(self, game):
+        self.game = game
+
+    def new(self, point):
+        column = FlappyColumn(self.game)
+        column.new(point)
+        self.columns.append(column)
+
+    def update(self):
+        for c in self.columns:
+            c.update() 
+
 
 class FlappyGame(Widget):
     bird = ObjectProperty(None)
-    columns = ListProperty(None)
+    points = NumericProperty(0)
+
+    def __init__(self):
+        super(FlappyGame, self).__init__()
+        self.columns = FlappyColumns(self)
 
     def update(self, dt):
         self.bird.move()
+        self.columns.update()
 
     def new_column(self, dt):
-        pass
+        self.columns.new(self.points * 1.1)
 
     def on_touch_down(self, touch):
         self.bird.reset()
@@ -61,7 +108,7 @@ class FlappyApp(App):
     def build(self):
         game = FlappyGame()
         Clock.schedule_interval(game.update, 1/60)
-        Clock.schedule_interval(game.new_column, 2)
+        Clock.schedule_interval(game.new_column, 1.5)
         return game
 
 if __name__ == '__main__':
